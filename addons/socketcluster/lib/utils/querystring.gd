f86cbc403ac2a6a6c2a8597cb7c023c69c06e006
Kevin Boon <kevin_boon@outlook.com>
2020-06-15 22:56:27 -0400

    Added querystring function

--- addons/socketcluster/lib/utils/querystring.gd
+++ addons/socketcluster/lib/utils/querystring.gd
@@ -2,5 +2,13 @@ extends Node
 
 class_name querystring
 
-static func encode(data):
-	pass
+static func encode(obj, sep = '&', eq = '=', name = ''):
+	var query = ''
+	if typeof(obj) == TYPE_DICTIONARY:
+		for key in obj:
+			if typeof(obj[key]) == TYPE_DICTIONARY:
+				query = query + encode(obj[key]) + sep
+			else:
+				query = query + key + eq + obj[key] + sep
+	query.erase(query.length() - 1, 1)
+	return query
