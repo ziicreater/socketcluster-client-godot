d9f235144e8493a40bb13911c3caff2669a98ab6
Kevin Boon <kevin_boon@outlook.com>
2020-06-19 18:49:45 -0400

    Added Decode function querystring

--- addons/socketcluster/lib/utils/querystring.gd
+++ addons/socketcluster/lib/utils/querystring.gd
@@ -2,7 +2,7 @@ extends Node
 
 class_name querystring
 
-static func encode(obj, sep = '&', eq = '=', name = ''):
+static func encode(obj, sep = '&', eq = '=', name = '') -> String :
 	var query = ''
 	if typeof(obj) == TYPE_DICTIONARY:
 		for key in obj:
@@ -12,3 +12,43 @@ static func encode(obj, sep = '&', eq = '=', name = ''):
 				query = query + key + eq + obj[key] + sep
 	query.erase(query.length() - 1, 1)
 	return query
+
+static func decode(qs, sep = '&', eq = '=', options = {}) -> Dictionary :
+	var obj = {}
+	
+	if typeof(qs) != TYPE_STRING && qs.length == 0:
+		return obj
+	
+	var regex = RegEx.new()
+	regex.compile("\\s+")
+	qs = qs.split(sep)
+	
+	var maxKeys = 1000
+	if options != {} && options.has('maxKeys') && options['maxKeys'] == TYPE_INT:
+		maxKeys = options['maxKeys']
+	
+	var le = qs.size()
+	if maxKeys > 0 && le > maxKeys:
+		le = maxKeys
+	
+	for i in range(0, le):
+		var x = regex.sub(qs[i], '+', true)
+		var idx = x.split(eq)
+		var kstr
+		var vstr
+		
+		if idx.size() >= 0:
+			kstr = idx[0]
+			vstr = idx[1]
+		else:
+			kstr = idx[0]
+			vstr = ''
+		
+		obj[kstr] = vstr
+		
+	return obj
+	
+	
+	
+	
+	
